45b587aebb95f8af9ed80f078629305bd34f6430
pradiptadani <pradiptadaniswara@student.uns.ac.id>
2023-03-14 01:30:45 +0700

    Menambahkan exercise_01.py

---
+++ exercise_01.py
@@ -0,0 +1,10 @@
+nama = 'Pradipta Daniswara'
+nim = 'I0322097'
+asal = 'Pekanbaru'
+tempat_tinggal = 'Saya tinggal di jalan embarkasi haji 64'
+visual_kampung = 'Kampung halaman saya yaitu Pekanbaru merupakan ibu kota dari provinsi Riau dan merupakan salah satu pusat ekonomi yang ada di pulau Sumatera'
+hope = 'Harapan saya di teknik industri ini adalah untuk menjadi seorang engineer yang baik dan mendapatkan pengalaman dan ilmu yang dapat diterapkan nantinya untuk kepentingan umum'
+
+print('Nama:', nama, '\n'+ 'NIM:',nim, '\n' + 'Asal:', asal)
+print('---------------------------------------------------')
+print(tempat_tinggal, '\n' + visual_kampung, '\n' + hope) 
